60b2ca8759a1253da2e71477a59ef9016e644731
amosteandai <amostendai6@gmail.com>
2025-02-16 12:50:48 +0200

    Adding cross screen navigation

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:meal_app/data/dummy_data.dart';
+import 'package:meal_app/screens/meals.dart';
 import 'package:meal_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: 'some title',
+          meals: [],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +35,9 @@ class CategoriesScreen extends StatelessWidget {
           for (final category in availableCategories)
             CategoryGridItem(
               category: category,
+              onSelectCategory: () {
+                _selectCategory(context);
+              },
             )
         ],
       ),
